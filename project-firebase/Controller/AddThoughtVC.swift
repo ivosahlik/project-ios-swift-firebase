5d28b21261502edc41a971b2a193c80f780f9fb4
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-11-19 21:21:58 +0100

    added mvc

---
+++ project-firebase/Controller/AddThoughtVC.swift
@@ -0,0 +1,34 @@
+//
+//  AddThoughtVC.swift
+//  project-firebase
+//
+//  Created by Ivo Vošahlík on 19/11/2018.
+//  Copyright © 2018 Ivo Vošahlík. All rights reserved.
+//
+
+import UIKit
+
+class AddThoughtVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ project-firebase/Controller/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  project-firebase
+//
+//  Created by Ivo Vošahlík on 19/11/2018.
+//  Copyright © 2018 Ivo Vošahlík. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
