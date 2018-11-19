7dfae669f40ef67db335d22ef23b9c79847ce6ee
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-11-19 16:58:16 +0100

    Initial Commit

---
+++ project-firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project-firebase
+//
+//  Created by Ivo Vošahlík on 19/11/2018.
+//  Copyright © 2018 Ivo Vošahlík. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
