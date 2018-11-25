41c2ae020da7cdc5eb3cf01a7bcb2a1507e338ab
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-11-25 23:07:17 +0100

    added view ThoughtCell

---
+++ project-firebase/View/ThoughtCell.swift
@@ -0,0 +1,18 @@
+//
+//  ThoughtCell.swift
+//  project-firebase
+//
+//  Created by Ivo Vošahlík on 25/11/2018.
+//  Copyright © 2018 Ivo Vošahlík. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
