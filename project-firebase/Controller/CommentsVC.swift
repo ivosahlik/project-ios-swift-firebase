deae73cb8635dacfe13aeb19b78b2654861b47b9
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-12-04 17:09:36 +0100

    added CommentsVC

---
+++ project-firebase/Controller/CommentsVC.swift
@@ -0,0 +1,19 @@
+//
+//  CommentsVC.swift
+//  project-firebase
+//
+//  Created by Ivo Vošahlík on 04/12/2018.
+//  Copyright © 2018 Ivo Vošahlík. All rights reserved.
+//
+
+import UIKit
+
+class CommentsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
